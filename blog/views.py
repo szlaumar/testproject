10e7d5414ce1865be6c24c81fb90afb905594886
Martin Szlauer <mszlauer@gemsystem.cz>
2016-09-07 14:19:53 +0200

    Přidána dynamika.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_data__lte=timezone.now()).order_by('published_data')
+    return render(request, 'blog/post_list.html', {'posts':posts})
