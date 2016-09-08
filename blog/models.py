33d5c992bf9368eac76cb7042f7de9a0bcbf9405
Martin Szlauer <mszlauer@gemsystem.cz>
2016-09-08 15:52:00 +0200

    Dodání další funkcionality. Změna jména sloupečku u Post. Přidán migrační script.

--- blog/models.py
+++ blog/models.py
@@ -9,10 +9,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_data = models.DateTimeField(default=timezone.now)
-    published_data = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_data = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
